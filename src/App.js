6ab5d37c40254c0bae6c4d7a0613a352b414c56a
SoleaMD <soleart00@gmail.com>
2022-08-20 17:39:42 +0200

    Added functionality to the Theme buttons
    -Moved Theme components (both .js and .css) tp the App.js component.
    -Added root variables to CSS and modified components to account for these variables.
    -Added dark theme option.
    -Theme buttons are now functional.
    -Added npm use-local-storage.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from "react";
 import Search from "./Search";
-import Theme from "./Theme";
 import Footer from "./Footer";
+import useLocalStorage from "use-local-storage";
 
 import "./App.css";
 
 export default function App() {
+  const [theme, setTheme] = useLocalStorage(
+    "theme" ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme =
+      theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
       <div className="container">
         <Search defaultCity="Madrid" />
-        <Theme />
+        <div className="Theme" data-theme={theme}>
+          <div className="theme-title">Theme</div>
+          <div className="row">
+            <div className="col-6 light-button">
+              <button
+                type="button"
+                className="btn btn-outline-info light mx-auto"
+                onClick={switchTheme}
+              >
+                Light
+              </button>
+            </div>
+            <div className="col-6 dark-button">
+              <button
+                type="button"
+                className="btn btn-outline-dark dark mx-auto"
+                onClick={switchTheme}
+              >
+                Dark
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
